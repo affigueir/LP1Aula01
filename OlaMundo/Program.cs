c2841dd4dc23ffd468508c0188defb1c97d7546b
André Furão Figueira <affigueir@gmail.com>
2020-02-27 10:26:59 +0000

    Add tags

--- OlaMundo/Program.cs
+++ OlaMundo/Program.cs
@@ -2,9 +2,17 @@
 
 namespace OlaMundo
 {
+    /// <summary>
+    /// Creates the Program
+    /// </summary>
     class Program
     {
+        /// <summary>
+        /// Main Function
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
+        //Fetch args from command line
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Bye World :(");
