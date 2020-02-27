1e5d6cdd73d3d39f1bb89ec19e32c0f10e473400
André Furão Figueira <affigueir@gmail.com>
2020-02-27 11:41:24 +0000

    Add string types

---
+++ VariasStrings/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace VariasStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Mom = "Wheres Dad?";
+            string Son = @"( ͡° ͜ʖ ͡°)";
+            string Sister = "\"Kill Dad he says.\"";
+            string Dog = @"""/Woof\?""";
+
+            Console.WriteLine(Mom);
+            Console.WriteLine(Son);
+            Console.WriteLine(Sister);
+            Console.WriteLine(Dog);
+            
+            
+        }
+    }
+}
