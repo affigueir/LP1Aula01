f9563d62553b373f9be7b5ff3d60b5f65be4dc83
André Furão Figueira <affigueir@gmail.com>
2020-02-27 11:03:07 +0000

    Add double

---
+++ TiposVarios/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace TiposVarios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Bob = 1;
+            int Mary = 10;
+            int Charles = 6;
+
+            double Donald = 0.15;
+            double Bernie = 0.90;
+
+            Console.WriteLine(Bob);
+            Console.WriteLine(Mary);
+            Console.WriteLine(Charles);
+            Console.WriteLine(Donald);
+            Console.WriteLine(Bernie);
+        }
+    }
+}
